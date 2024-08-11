c16d834ea28178c0c4b8027f86479e7f6810a76b
HemalRathod25 <hprathod047@gmail.com>
2024-08-11 22:41:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public  class Hello{
     public static void main(String[] args) {
-        System.out.println("hello new java");
+        System.out.println("Hello Word This Is My First Java Program :");
     }
 }
